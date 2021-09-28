4452e71232b13b95b8373487b0bb0a6a5c7b4175
dr-van-nostrand <lzubeldia@gmail.com>
2021-09-28 18:57:52 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Counter from "./Counter";
+import CounterHooks from "./CounterHooks";
+
+export const ThemeContext = React.createContext();
 
 function App() {
+  const [theme, setTheme] = useState("blue");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
+      First Counter
+      <Counter initialcount={100} />
+      <br />
+      Second Counter Hooks
+      <CounterHooks initialCounter={10} />
+      <button
+        onClick={() =>
+          setTheme((prevTheme) => {
+            return prevTheme === "blue" ? "tomato" : "blue";
+          })
+        }
+      >
+        color
+      </button>
+    </ThemeContext.Provider>
   );
 }
 
